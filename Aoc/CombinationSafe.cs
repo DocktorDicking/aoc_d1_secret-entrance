e1ef305869201f0f700c252a5241f39bd649e410
Jim <jvw91@msn.com>
2025-12-14 20:46:06 +0100

    Add `_startValue` to `CombinationSafe` and initialize `CombinationDial` with it

--- Aoc/CombinationSafe.cs
+++ Aoc/CombinationSafe.cs
@@ -4,18 +4,25 @@ public class CombinationSafe : ICombinationSafe
 {
     private readonly CombinationDial _dial;
     private readonly int _maxValue = 99;
+    private readonly int _startValue = 50;
 
     public CombinationSafe()
     {
-        _dial = new CombinationDial(_maxValue);
+        _dial = new CombinationDial(_maxValue, _startValue);
     }
     
     public int TurnCombinationDial(Direction direction, int targetValue)
     {
         if (targetValue > _maxValue)
+            // Fun fact, using string interpolation with the logger will create a new string every time. Structured logging is the way to make that more optimized.
             throw new ArgumentOutOfRangeException(nameof(targetValue), targetValue, $"Target value must be less than or equal to {_maxValue}.");
         
         return _dial.TurnDialTo(direction, targetValue);
+    }
+
+    public int GetStatistics()
+    {
+        return 0;
     }
 }
 
