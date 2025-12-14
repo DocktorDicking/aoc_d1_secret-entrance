d6b5921d1f13f32975444bf83d8572babdeb80fe
Jim <jvw91@msn.com>
2025-12-14 20:21:02 +0100

    Add dependency injection and `App` entry point for application runtime

--- Aoc/Program.cs
+++ Aoc/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Aoc;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+// Register services
+services.AddSingleton<CombinationSafe>();
+services.AddTransient<App>();
+
+using var provider = services.BuildServiceProvider();
+
+provider.GetRequiredService<App>().Run(args);
+
+
+
+public sealed class App
+{
+    private readonly CombinationSafe _safe;
+
+    public App(CombinationSafe safe) => _safe = safe;
+
+    public void Run(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Starting dial:");
+            Console.WriteLine(_safe.TurnCombinationDial(Direction.Right, 100));
+        }
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Exception thrown and caught: ");
+            Console.WriteLine(e);
+        }
+    }
+}
