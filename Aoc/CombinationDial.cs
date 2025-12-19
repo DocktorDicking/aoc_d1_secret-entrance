5fe61f34f764ddf253ad6fd5968f37ca9b5b47b4
Jim <jvw91@msn.com>
2025-12-19 12:12:41 +0100

    Refactor `CombinationDial` and `CombinationSafe` to replace recursive dial turning with iterative logic, update event tracking to use tick counts instead of target values

--- Aoc/CombinationDial.cs
+++ Aoc/CombinationDial.cs
@@ -12,50 +12,35 @@ public class CombinationDial : ICombinationDial
         MaxValue = maxValue;
         _currentValue = startValue;
     }
-    
-    /**
-     * Recursion is not the best way to solve this problem.
-     * Apparently, each recursive call will be a new stack frame (function call + keeping track of the return address).
-     * This is also considered riskier than iterative solutions.
-     * But since this is for fun, recursion goes brrrr.
-     */
-    public int TurnDialTo(Direction direction, int target)
-    {
-        if (_currentValue == target)
-        {
-            return _currentValue;
-        }
-        
-        var startValue = _currentValue;
-        TurnDial(direction);
-        dialEventListner.TrackEvent(direction, target, startValue, _currentValue);
-        return TurnDialTo(direction, target);
-    }
 
     public List<string> ReadEvents()
     {
         return dialEventListner.PrintEvents();
     }
     
-    private void TurnDial(Direction direction)
+    public void TurnDial(Direction direction, int ticks)
     {
-        switch (direction)
+        for (int i = 0; i < ticks; i++)
         {
-            case Direction.Left when _currentValue == 0:
-                _currentValue = MaxValue;
-                return;
-            case Direction.Left:
-                _currentValue--;
-                return;
-            case Direction.Right when _currentValue == MaxValue:
-                _currentValue = _minValue;
-                return;
-            case Direction.Right:
-                _currentValue++;
-                return;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            switch (direction)
+            {
+                case Direction.Left when _currentValue == 0:
+                    _currentValue = MaxValue;
+                    break;
+                case Direction.Left:
+                    _currentValue--;
+                    break;
+                case Direction.Right when _currentValue == MaxValue:
+                    _currentValue = _minValue;
+                    break;
+                case Direction.Right:
+                    _currentValue++;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
         }
+        dialEventListner.TrackEvent(direction, _currentValue, _minValue, MaxValue);
     }
 }
 
@@ -63,13 +48,13 @@ public class DialEventListner
 {
     private List<DialEvent> _eventsList = new();
 
-    public void TrackEvent(Direction direction, int target, int startValue, int endValue)
+    public void TrackEvent(Direction direction, int ticks, int startValue, int endValue)
     {
         _eventsList.Add(
             new DialEvent
             {
                 Direction = direction, 
-                Target = target, 
+                Ticks = ticks, 
                 StartValue = startValue, 
                 EndValue = endValue
             });
@@ -81,24 +66,17 @@ public class DialEventListner
 
         foreach (var dialEvent in _eventsList)
         {
-            lines.Add($"Direction: {dialEvent.Direction}, Target: {dialEvent.Target}, StartValue: {dialEvent.StartValue}, EndValue: {dialEvent.EndValue}");
+            lines.Add($"Direction: {dialEvent.Direction}, Target: {dialEvent.Ticks}, StartValue: {dialEvent.StartValue}, EndValue: {dialEvent.EndValue}");
         }
 
         return lines;
     }
-
-    public void PrintPassword()
-    {
-        //TODO: Filter list of events to only have events that passed the 0 at some point.
-        throw new NotImplementedException("Not implemented yet.");
-    }
-
 }
 
 internal class DialEvent
 {
     public Direction Direction { get; set; }
-    public int Target { get; set; }
+    public int Ticks { get; set; }
     public int StartValue { get; set; }
     public int EndValue { get; set; }
     public int Turns { get; set; }
--- Aoc/CombinationSafe.cs
+++ Aoc/CombinationSafe.cs
@@ -5,28 +5,27 @@ public class CombinationSafe : ICombinationSafe
     private readonly CombinationDial _dial;
     private readonly int _maxValue = 99;
     private readonly int _startValue = 50;
+    private readonly List<(Direction Direction, int Ticks)> _turns;
 
     public CombinationSafe()
     {
         _dial = new CombinationDial(_maxValue, _startValue);
+        _turns = [];
     }
     
-    public int TurnCombinationDial(Direction direction, int targetValue)
+    public CombinationSafe AddDialTurn(Direction direction, int ticks)
     {
-        if (targetValue > _maxValue)
-            // Fun fact, using string interpolation with the logger will create a new string every time. Structured logging is the way to make that more optimized.
-            throw new ArgumentOutOfRangeException(nameof(targetValue), targetValue, $"Target value must be less than or equal to {_maxValue}.");
-        
-        return _dial.TurnDialTo(direction, targetValue);
+        _turns.Add((direction, ticks));
+        return this;
+    }
+
+    public void Run()
+    {
+        _turns.ForEach(turn => _dial.TurnDial(turn.Direction, turn.Ticks));
     }
 
     public List<string> GetStatistics()
     { 
         return _dial.ReadEvents();
     }
-}
-
-public interface ICombinationSafe
-{
-    
 }
